18dc237f48406f939cbabd452fd29581f6aacaad
thbr06001 <thbr06001@E-PW0234MD>
2023-09-06 10:15:11 +0200

    Add project files.

---
+++ Inlämningsuppgift kap 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Uppgift1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1"); //Skriver att jag nästan är klar med kapitel 1
+            Console.WriteLine("Du har lärt dig att skriva ut tecken som \" och \\ "); //Skriver att jag lärt mig skriva ut nya tecken 
+        }
+    }
+}
